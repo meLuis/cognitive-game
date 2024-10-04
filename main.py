fd64a727bb8108a86de1e1ed424c8558ff956076
meLuis <luisespinozanav024@gmail.com>
2024-10-04 12:59:55 -0600

    puntajes agregados

--- main.py
+++ main.py
@@ -41,11 +41,30 @@ def calcular_puntaje_bonus(jugador,puntaje,cant_bonus,pares):
     print(f"El jugador {jugador} ha encontrado una pareja bonus\n")
     return [puntaje,pares,cant_bonus]
 
+def validar_final_normal(pares_encontrados,elementos,cant_bonus_1,cant_bonus_2,pares_1,pares_2):
+    if pares_encontrados==elementos//2:
+        print("El juego ha terminado")
+        if cant_bonus_1>cant_bonus_2:
+            print("El ganador es el jugador 1")
+            print("Bonus points multiplicados por el total de pares destapados: ",cant_bonus_1*pares_1)
+        elif cant_bonus_1<cant_bonus_2:
+            print("El ganador es el jugador 2")
+            print("Bonus points multiplicados por el total de pares destapados: ",cant_bonus_2*pares_2)
+        else:
+            print("El juego ha terminado en empate")
+
+        return 'no'
+    return 'si'
+
+def validar_final_forzado(pares_1,pares_2,puntos_1,puntos_2,cant_bonus_1,cant_bonus_2):
+    print("El juego se ha detenido")
+
 def main():
     m_cuadrada=6
     elementos=pow(m_cuadrada,2)
     simbolo="*"
     jugar='si'
+    pares_encontrados=0
     jugador=0
     cant_bonus_1=0
     cant_bonus_2=0
@@ -55,16 +74,13 @@ def main():
     pares_2=0
     matriz=crear_matriz(m_cuadrada,elementos)
     matriz_volteada=crear_matriz_volteada(simbolo, m_cuadrada,elementos)
-    dibujar_matriz(matriz,m_cuadrada)
-    time.sleep(3)
-    os.system('clear')
     dibujar_matriz(matriz_volteada,m_cuadrada)
 
     while jugar=='si':
 
         jugador=jugador%2+1
 
-        print(f"Turno del jugador {jugador}\n")
+        print(f"\nTurno del jugador {jugador}\n")
 
         print("Ingrese la fila y columna de la primera carta")
         fila_indicada_1=int(input("Ingrese la fila: "))-1
@@ -84,7 +100,10 @@ def main():
         valor_2=matriz[fila_indicada_2][columna_indicada_2]
 
         if valor_1==valor_2:
+            pares_encontrados+=1
             if valor_1%5==0:
+                print(f"El jugador {jugador} ha encontrado una par bonus\n")
+
                 if jugador==1:
                     lista_bonus=calcular_puntaje_bonus(jugador,puntos_1,cant_bonus_1,pares_1)
                     puntos_1=lista_bonus[0]
@@ -96,6 +115,7 @@ def main():
                     pares_2=lista_bonus[1]
                     cant_bonus_2=lista_bonus[2]
             else:
+                print(f"El jugador {jugador} ha encontrado una par\n")
                 if jugador==1:
                     lista_normal=calcular_puntaje_normal(jugador,puntos_1,pares_1)
                     puntos_1=lista_normal[0]
@@ -104,14 +124,20 @@ def main():
                     lista_normal=calcular_puntaje_normal(jugador,puntos_2,pares_2)
                     puntos_2=lista_normal[0]
                     pares_2=lista_normal[1]
-
         else:
-            time.sleep(2)
+            print(f"El jugador {jugador} no ha encontrado una par\n")
             matriz_volteada[fila_indicada_1][columna_indicada_1]=simbolo
             matriz_volteada[fila_indicada_2][columna_indicada_2]=simbolo
 
 
-        jugar=input("Desea seguir jugando? si/no: ")
+        jugar=validar_final_normal(pares_encontrados,elementos,cant_bonus_1,cant_bonus_2,pares_1,pares_2)
+
+        if jugador==2 and jugar!="no":
+            jugar=input("Desea seguir jugando? si/no: ")
+            if jugar=="no":
+                validar_final_forzado(pares_1,pares_2,puntos_1,puntos_2,cant_bonus_1,cant_bonus_2)
+
+    
 
 if __name__=='__main__':
     main()
